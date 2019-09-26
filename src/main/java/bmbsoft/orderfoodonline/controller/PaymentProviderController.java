29a3fc95bf927d1cedf1f2428a6391d70038098e
Tu Bean <tu.do@bmbsoft.com.vn>
2019-09-26 13:43:19 +0700

    create new api for payment Provider

--- src/main/java/bmbsoft/orderfoodonline/controller/PaymentProviderController.java
+++ src/main/java/bmbsoft/orderfoodonline/controller/PaymentProviderController.java
@@ -1,44 +1,17 @@
 package bmbsoft.orderfoodonline.controller;
 
-import java.util.Date;
-import java.util.List;
-
+import bmbsoft.orderfoodonline.model.PaymentProviderViewModel;
+import bmbsoft.orderfoodonline.response.ResponseGet;
+import bmbsoft.orderfoodonline.service.PaymentProviderService;
+import bmbsoft.orderfoodonline.util.Constant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import bmbsoft.orderfoodonline.entities.PaymentProvider;
-import bmbsoft.orderfoodonline.entities.Restaurant;
-import bmbsoft.orderfoodonline.entities.Category;
-import bmbsoft.orderfoodonline.entities.District;
-import bmbsoft.orderfoodonline.entities.User;
-import bmbsoft.orderfoodonline.model.AddressViewModel;
-import bmbsoft.orderfoodonline.model.PaymentProviderViewModel;
-import bmbsoft.orderfoodonline.model.shared.CategoryRequest;
-import bmbsoft.orderfoodonline.model.shared.DeleteManyRequest;
-import bmbsoft.orderfoodonline.model.shared.PaymentProviderRequest;
-import bmbsoft.orderfoodonline.response.Data;
-import bmbsoft.orderfoodonline.response.ResponseGet;
-import bmbsoft.orderfoodonline.response.ResponseGetPaging;
-import bmbsoft.orderfoodonline.service.DistrictService;
-import bmbsoft.orderfoodonline.service.PaymentProviderService;
-import bmbsoft.orderfoodonline.service.PaymentProviderService;
-import bmbsoft.orderfoodonline.service.ResidenceTypeService;
-import bmbsoft.orderfoodonline.service.RestaurantService;
-import bmbsoft.orderfoodonline.service.UserService;
-import bmbsoft.orderfoodonline.util.CommonHelper;
-import bmbsoft.orderfoodonline.util.Constant;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -49,7 +22,7 @@ public class PaymentProviderController {
 	PaymentProviderService paymentProviderService;
 
 	@RequestMapping(value = "/api/PaymentProvider/getAll", method = RequestMethod.GET)
-	public ResponseEntity<?> getAllProvider(@PathVariable long id) {
+	public ResponseEntity<?> getAllProvider() {
 		ResponseGet rs = new ResponseGet();
 		try {
 			List<PaymentProviderViewModel> c = paymentProviderService.getAllProviders();
